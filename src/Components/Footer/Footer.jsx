299c19f4ea695291edad5ec3f4cf5d7a6af730d9
Mohri Samuel Muakpo <sammuaks@gmail.com>
2025-01-03 12:00:26 +0100

    Fix login page for wider screens

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -45,7 +45,7 @@ function Footer() {
 
       <div className="footer-copyright">
         <hr />
-        <p>Sam-Muaks ©️ {year} 2023 - All Rights Reserved</p>
+        <p>Sam-Muaks ©️ {year} - All Rights Reserved</p>
       </div>
     </div>
   );
