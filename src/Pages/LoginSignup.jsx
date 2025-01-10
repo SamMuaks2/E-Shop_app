63e6369c76a0f4d919f8570b421ae20c99a935a7
Mohri Samuel Muakpo <sammuaks@gmail.com>
2025-01-10 16:13:15 +0100

    Add Login/Sign up state management

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CSS/LoginSignup.css";
 
 function LoginSignup() {
+
+  const [state, setState] = useState("Login");
+
   return (
     <div className="loginsignup">
 
       <div className="loginsignup-container">
       
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
       
         <div className="loginsignup-fields">
-          <div className="input-name">
-          <input type="text" placeholder="First Name" />
-          <input type="text" placeholder="Surname" /> 
-          </div>
+          {state === "Sign Up" ? <div className="input-name">
+            <input type="text" placeholder="First Name" />
+            <input type="text" placeholder="Surname" /> 
+          </div> : <></> }
 
           <input type="email" placeholder="Email Address" />
           <input type="password" placeholder="Password" />
@@ -21,9 +24,18 @@ function LoginSignup() {
       
         <button>Continue</button>
 
+        {state === "Sign Up" ? 
+        
         <p className="loginsignup-login">
-          Already have an account? <span>Login here</span>
+          Already have an account? <span onClick={() => {setState("Login")}}>Login here</span>
         </p>
+
+        :
+
+        <p className="loginsignup-login">
+          Create an account? <span onClick={() => {setState("Sign Up")}}>Click here</span>
+        </p>
+        }
 
         <div className="loginsignup-agree">
           <input type="checkbox" name="" id="" />
