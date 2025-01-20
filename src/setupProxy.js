890d5b96d6be4f84edc3769c3d9789a6e43ba31f
Mohri Samuel Muakpo <sammuaks@gmail.com>
2025-01-20 14:37:00 +0100

    Use proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,16 +1,16 @@
 // src/setupProxy.js
-// const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// module.exports = function (app) {
-//   app.use(
-//     '/api',
-//     createProxyMiddleware({
-//       target: 'http://localhost:4000',
-//       changeOrigin: true,
-//       onProxyRes: function (proxyRes) {
-//         proxyRes.headers['Content-Security-Policy'] =
-//           "default-src 'self'; connect-src 'self' http://localhost:4000; script-src 'self'";
-//       },
-//     })
-//   );
-// };
+module.exports = function (app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://e-shop-backend-eta.vercel.app',
+      changeOrigin: true,
+      onProxyRes: function (proxyRes) {
+        proxyRes.headers['Content-Security-Policy'] =
+          "default-src 'self'; connect-src 'self' https://e-shop-backend-eta.vercel.app; script-src 'self'";
+      },
+    })
+  );
+};
