134308bbafc15aab26da0277ba171cadd66e4c8f
Mohri Samuel Muakpo <sammuaks@gmail.com>
2025-01-23 18:39:54 +0100

    Update product page

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -2,13 +2,13 @@ import React, { useContext } from "react";
 import { ShopContext } from "../Context/ShopContext";
 import { useParams } from "react-router-dom";
 import Breadcrumb from "../Components/Breadcrumbs/Breadcrumb";
-import all_product from "../Components/Assets/all_product";
+// import all_product from "../Components/Assets/all_product";
 import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
 import Descriptionbox from "../Components/DescriptionBox/Descriptionbox";
 import RelatedProducts from "../Components/RelatedProducts/RelatedProducts";
 
 const Product = () => {
-  const { all_products } = useContext(ShopContext);
+  const { all_product } = useContext(ShopContext);
   const { productId } = useParams();
   const product = all_product.find((e) => e.id === Number(productId));
   return (
